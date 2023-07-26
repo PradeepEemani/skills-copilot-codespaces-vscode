4fe924d8a8b848bc4075ff98085f292c7e0f7a54
Pradeep Eemani <preemani@microsoft.com>
2023-07-25 21:25:04 -0700

    Co-Pilot Second Commit

---
+++ members.js
@@ -0,0 +1,21 @@
+function skillsMembers() {
+  const members = [
+    {
+      name: 'John Doe',
+      age: 25,
+      skills: ['JavaScript', 'CSS', 'HTML'],
+    },
+    {
+      name: 'Jane Doe',
+      age: 28,
+      skills: ['JavaScript', 'CSS', 'HTML', 'Node.js'],
+    },
+    {
+      name: 'Jack Doe',
+      age: 22,
+      skills: ['JavaScript', 'CSS'],
+    },
+  ];
+
+  return members;
+}
